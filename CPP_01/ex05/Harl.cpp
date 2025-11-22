d074967e592a9cfbece546a67eee85558520d58b
TiagoDev88 <tiagomonteiro.dev@gmail.com>
2025-11-22 22:31:12 +0000

    Implement Harl class with debug, info, warning, and error methods; update notas with message levels and examples

---
+++ CPP_01/ex05/Harl.cpp
@@ -0,0 +1,35 @@
+
+
+
+#include "Harl.hpp"
+
+Harl::Harl(void) {};
+
+Harl::~Harl(void) {};
+
+void Harl::debug( void )
+{
+
+}
+
+void Harl::info( void )
+{
+
+}
+
+void Harl::warning( void )
+{
+
+}
+
+void Harl::error( void )
+{
+
+}
+
+
+
+void Harl::complain( std::string level )
+{
+    
+}
